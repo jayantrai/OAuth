e87f74428c9ab6ae0e9b43fec9d2895e26542add
Jayant Rai <jayant.chamling@outlook.com>
2020-05-24 19:46:24 -0500

    Project setup using SpringWeb and SpringSecurity dependency

---
+++ OAuth/OAuth-FaceBook/src/test/java/com/example/OAuthFaceBook/OAuthFaceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuthFaceBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthFaceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
